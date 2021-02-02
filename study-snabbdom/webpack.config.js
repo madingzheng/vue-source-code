177d65db6823ee51281509d833209f4fd2aed28b
Mdz <747016839@qq.com>
2021-02-02 10:52:51 +0800

    init snabbdom

--- study-snabbdom/webpack.config.js
+++ study-snabbdom/webpack.config.js
@@ -1,29 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        // ts-loader是官方提供的处理tsx的文件
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: false,
     port: 9000,
     publicPath: '/xuni/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
   }
-}
-;
+};
