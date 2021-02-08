137b33f4c736c4a9884834bb9771b2001b722f20
Mdz <747016839@qq.com>
2021-02-08 23:18:05 +0800

    init

--- study-data-reactive/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export interface MyObj {
-  [propName: string]: any
-}
--- study-data-reactive/webpack.config.js
+++ study-data-reactive/webpack.config.js
@@ -1,30 +1,20 @@
+// 从https://www.webpackjs.com/官网照着配置
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        // ts-loader是官方提供的处理tsx的文件
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: false,
-    port: 9000,
-    publicPath: '/xuni/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  }
-}
-;
+    // 入口
+    entry: './src/index.js',
+    // 出口
+    output: {
+        // 虚拟打包路径，就是说文件夹不会真正生成，而是在8080端口虚拟生成
+        publicPath: 'xuni',
+        // 打包出来的文件名，不会真正的物理生成
+        filename: 'bundle.js'
+    },
+    devServer: {
+        // 端口号
+        port: 8080,
+        // 静态资源文件夹
+        contentBase: 'www'
+    }
+};
